8cda43126c20cb99f9932a8d0867040f1789c279
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-16 12:58:25 +0100

    Set common js file for templates

--- template/js/book_event.js
+++ template/js/book_event.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-  $(".button-collapse").sideNav();
-  $('select').material_select();
+  $(".button-collapse").sideNav(); //for nav menu on small screens
+  // for carousel elements
+  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+  $('select').material_select(); // for select tags
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
   //reinitialize all the Materialize labels on the form if you are dynamically adding inputs
   Materialize.updateTextFields();
 })
