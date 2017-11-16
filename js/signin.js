1b53623a547698d6c0365fb231298ca0ec1f02b0
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-16 13:05:27 +0100

    Link templates to shared js file

--- js/signin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".button-collapse").sideNav();
-  $('.carousel').carousel();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true
-  });
-  //reinitialize all the Materialize labels on the form if you are dynamically adding inputs
-  Materialize.updateTextFields();
-})
