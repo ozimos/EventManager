132ee8da3f6e06b21a2f86540d8abc8c6a8e8df3
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-12-29 22:09:29 +0100

    implement switch method

--- test/appTest.js
+++ test/appTest.js
@@ -1,54 +1,183 @@
-import { expect } from 'chai';
+import chai from 'chai';
 import supertest from 'supertest';
-import app from './server/app.js';
+import app from '../server/app.js';
 
+const chaiExpect = chai.expect;
 const request = supertest(app);
-const rootURL = '/api';
-const centerUrl = `${rootURL}/v1/centers`;
-const centerDetailUrl = `${rootURL}/v1/centers/1`;
-const editCenterUrl = `${rootURL}/v1/centers/1`;
-const addCenterUrl = `${rootURL}/v1/centers`;
-const addEventUrl = `${rootURL}/v1/events`;
-const editEventUrl = `${rootURL}/v1/events/1`;
+const rootURL = '/api/v1';
+const centersUrl = `${rootURL}/centers`;
+const centerIdUrl = `${rootURL}/centers/1`;
+const eventsUrl = `${rootURL}/events`;
+const eventsIdUrl = `${rootURL}/events/1`;
+
+const newCenter = {
+  id: 1,
+  name: 'Baranduil',
+  description: 'a  dark and dank castle shrouded in gloom',
+  cost: 50000,
+  capacity: 5000,
+  location: {
+    country: 'Nigeria',
+    state: 'Lagos',
+    lga: 'Oshodi'
+  },
+  amenities: ['Pool', 'Bar'],
+  eventType: ['Cocktail', 'Birthday']
+};
+
+const getRequest = request.get();
+const putRequest = request.put();
+const postRequest = request.post();
+const deleteRequest = request.delete();
+
+/**
+ * Generates new tests with a template
+ * @param {string} title
+ * @param {string} method
+ * @param {string} url
+ * @param {object} payload
+ * @param {string} key
+ *  @returns {function} mocha test suite
+ */
+const templateTest = function generateTest(title, method, url, payload, key) {
+  let reqMethod;
+  switch (method) {
+    case 'delete':
+      reqMethod = request.delete(url);
+      break;
+    case 'put':
+      reqMethod = request.put(url);
+      break;
+    case 'post':
+      reqMethod = request.post(url);
+      break;
+    case 'get':
+    default:
+      reqMethod = request.get(url);
+  }
+
+  return () => {
+    describe(title, () => {
+      it('return 200 for successful', (done) => {
+        reqMethod
+          .send(payload)
+          .expect(200, done);
+      });
+      it('response should be json', (done) => {
+        reqMethod
+          .send(payload)
+          .expect('Content-Type', /json/, done);
+      });
+      it('response should be an object', (done) => {
+        reqMethod
+          .send(payload)
+          .end((err, res) => {
+            chaiExpect(res.json).to.be.an('object');
+            done();
+          });
+      });
+      it(`response should have keys: ${key} and error`, (done) => {
+        reqMethod
+          .send(payload)
+          .end((err, res) => {
+            chaiExpect(res.json).to.include.all.keys(`${key}`, 'error');
+            done();
+          });
+      });
+    });
+  };
+};
 
 
-describe('Test isSubset function, first array argument is subset of second array arggument', () => {
-  const arr3 = [3, 2, 1];
-
-  it('returns a boolean', () => {
-    const arr1 = [1, 2, 3, 4];
-    const arr2 = [3, 2, 1];
-    assert.isBoolean(isSubset(arr1, arr2), 'returns true or false');
-  });
-  describe('Test arrays of unequal length, not subset', () => {
-    it('first array longer than second array', () => {
-      const arr1 = [1, 2, 3, 4];
-      const arr2 = [3, 2, 1];
-      assert.equal(isSubset(arr1, arr2), false, 'should return false');
+describe('API Integration Tests', () => {
+  describe('Get All Centers', () => {
+    it('return 200 for successful', (done) => {
+      request.get(centersUrl)
+        .expect(200, done);
     });
-    it('first array shorter than second array ', () => {
-      const arr1 = [3, 2, 1];
-      const arr2 = [1, 2, 4, 5];
-      assert.equal(isSubset(arr1, arr2), false, 'should return false');
+    it('response should be json', (done) => {
+      request.get(centersUrl)
+        .expect('Content-Type', /json/, done);
     });
-  });
-  describe('Test arrays of unequal length, but subset', () => {
-    it('first array longer than second array', () => {
-      const arr1 = [1, 3, 2, 3];
-      const arr2 = [3, 2, 1];
-      assert.equal(isSubset(arr1, arr2), true, 'should return true');
+    it('response should be an object', (done) => {
+      request.get(centersUrl)
+        .end((err, res) => {
+          chaiExpect(res.json).to.be.an('object');
+          done();
+        });
     });
-    it('first array shorter than second array ', () => {
-      const arr1 = [3, 2, 1];
-      const arr2 = [1, 2, 3, 4];
-      assert.equal(isSubset(arr1, arr2), true, 'should return true');
+    it('response should have required keys', (done) => {
+      request.get(centersUrl)
+        .end((err, res) => {
+          chaiExpect(res.json).to.include.all.keys('message', 'centers', 'error');
+          done();
+        });
     });
   });
-  describe('Test arrays of unequal length containing strings, but subset', () => {
-    it('first array shorter than second array ', () => {
-      const arr1 = ['name', 'cost', 'capacity', 'image', 'country', 'state', 'lga', 'amenities', 'eventType'];
-      const arr2 = ['name', 'cost', 'capacity', 'image', 'country', 'state', 'lga', 'amenities', 'eventType'];
-      assert.equal(isSubset(arr1, arr2), true, 'should return true');
+  describe('Get Center Details', () => {
+    it('return 200 for successful', (done) => {
+      request.get(centerIdUrl)
+        .expect(200, done);
+    });
+    it('response should be json', (done) => {
+      request.get(centerIdUrl)
+        .expect('Content-Type', /json/, done);
+    });
+    it('response should be an object', (done) => {
+      request.get(centerIdUrl)
+        .end((err, res) => {
+          chaiExpect(res.json).to.be.an('object');
+          done();
+        });
+    });
+    it('response should have required keys', (done) => {
+      request.get(centerIdUrl)
+        .end((err, res) => {
+          chaiExpect(res.json).to.include.all.keys('message', 'center', 'error');
+          done();
+        });
+    });
+  });
+  describe('Modify Center Details', () => {
+    it('return 200 for successful', (done) => {
+      request.put(centerIdUrl)
+        .send({
+          name: 'Codex Alexera',
+          state: 'Abuja',
+        })
+        .expect(200, done);
+    });
+    it('response should be json', (done) => {
+      request.put(centerIdUrl)
+        .send({
+          name: 'Codex Alexera',
+          state: 'Abuja',
+        })
+        .expect('Content-Type', /json/, done);
+    });
+    it('response should be an object', (done) => {
+      request.put(centerIdUrl)
+        .send({
+          name: 'Codex Alexera',
+          state: 'Abuja',
+        })
+        .end((err, res) => {
+          chaiExpect(res.json).to.be.an('object');
+          done();
+        });
+    });
+    it('response should have required keys', (done) => {
+      request.put(centerIdUrl)
+        .send({
+          name: 'Codex Alexera',
+          state: 'Abuja',
+        })
+        .end((err, res) => {
+          chaiExpect(res.json).to.include.all.keys('message', 'center', 'error');
+          done();
+        });
     });
   });
 });
+
+templateTest('Add center', 'post', centerIdUrl, newCenter, 'center');
