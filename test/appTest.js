ed51957279c5650867bb3979f9d5a7239f9e438e
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-01-07 02:27:25 +0100

    final branch commit

--- test/appTest.js
+++ test/appTest.js
@@ -1,10 +1,11 @@
-import chai from 'chai';
+import {
+  expect
+} from 'chai';
 import supertest from 'supertest';
 import app from '../server/app.js';
 
-
-const chaiExpect = chai.expect;
-const request = supertest(app);
+const server = app.listen();
+const request = supertest(server);
 
 // endpoint urls
 const rootURL = '/api/v1';
@@ -46,49 +47,58 @@ const newEvent = {
 
 /**
  * Generates new tests with a template
- * @param {string} title
- * @param {string} method
- * @param {string} url
- * @param {object} payload
- * @param {string} key
- * @param {string} type
+ * @param {string} title    - Title of the test
+ * @param {string} method   - HTTP verb in lowercase e.g. get
+ * @param {string} url      - Request URL
+ * @param {object} payload  - Data to be sent to server
+ * @param {string} key      - key for requested data in res.body
+ * @param {string} type     - type of value for key above
  *  @returns {function} mocha test suite
  */
 
 const templateTest = function generateTest(title, method, url, payload, key, type) {
   describe(title, () => {
-    const postRequest = request[method].bind(request, url);
-    it('return 200 for successful', (done) => {
-      postRequest()
-        .send(payload)
-        .expect(200, done);
+    const Request = request[method].bind(request, url);
+    it('return 200 for successful', async () => {
+      try {
+        const response = await Request().send(payload);
+        expect(response.status).to.equal(200);
+      } catch (err) {
+        throw err;
+      }
     });
-    it('response should be json', (done) => {
-      postRequest()
-        .send(payload)
-        .expect('Content-Type', /json/, done);
+    it('response should be json', async () => {
+      try {
+        const response = await Request().send(payload);
+        expect(response.header).to.include.all.keys('content-type');
+        expect(response.header['content-type']).to.match(/json/);
+      } catch (err) {
+        throw err;
+      }
     });
-    it('response should have required keys', (done) => {
-      postRequest()
-        .send(payload)
-        .end((err, res) => {
-          chaiExpect(res.body).to.include.all.keys('message', key, 'error');
-          done();
-        });
+    it('response.body should have required keys', async () => {
+      try {
+        const response = await Request().send(payload);
+        expect(response.body).to.include.all.keys('message', key, 'error');
+      } catch (err) {
+        throw err;
+      }
     });
-    it('response should be an object', (done) => {
-      postRequest()
-        .send(payload)
-        .end((err, res) => {
-          chaiExpect(res.body[key]).to.be.an(type);
-          done();
-        });
+    it('response.body.key should be an object', async () => {
+      try {
+        const response = await Request().send(payload);
+        return expect(response.body[key]).to.be.a(type);
+      } catch (err) {
+        throw err;
+      }
     });
   });
 };
 
-
 describe('API Integration Tests', () => {
+  after(async () => {
+    await server.close();
+  });
   describe('Centers Endpoint Tests', () => {
     templateTest('Get All Centers', 'get', centersUrl, null, 'centers', 'array');
     templateTest('Get Center Details', 'get', centerIdUrl, null, 'center', 'object');
