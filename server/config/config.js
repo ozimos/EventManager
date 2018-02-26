5920a07a491aabaf3910d503d2cd3bf224d212e3
Tovieye Ozi <tovieye.ozi@gmail.com>
2018-02-26 18:41:26 +0100

    final config

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,7 @@ export default {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   }
 };
